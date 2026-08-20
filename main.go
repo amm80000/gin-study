be3bf876eb46450c08161d263b3baaee88d234c2
an'zhong'yuan <919423510@qq.com>
2026-08-20 17:42:41 +0800

    [FEAT]使用http方法

--- main.go
+++ main.go
@@ -6,16 +6,66 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func getting(c *gin.Context) {
+
+	c.JSON(http.StatusOK, gin.H{
+		"method": "GET",
+	})
+}
+
+func posting(c *gin.Context) {
+
+	c.JSON(http.StatusOK, gin.H{
+		"method": "POST",
+	})
+}
+
+func putting(c *gin.Context) {
+
+	c.JSON(http.StatusOK, gin.H{
+		"method": "PUT",
+	})
+}
+
+func deleting(c *gin.Context) {
+
+	c.JSON(http.StatusOK, gin.H{
+		"method": "DELETE",
+	})
+}
+
+func patching(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"method": "PATCH",
+	})
+}
+
+func head(c *gin.Context) {
+
+	c.JSON(http.StatusOK, gin.H{
+		"method": "HEAD",
+	})
+}
+
+func options(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"method": "OPTIONS",
+	})
+}
+
 func main() {
 
 	router := gin.Default()
 
-	router.GET("/ping", func(c *gin.Context) {
-
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
-
-	router.Run(":8081")
+	router.GET("/someGet", getting)
+	router.POST("/somePost", posting)
+	router.PUT("/somePut", putting)
+	router.DELETE("/someDelete", deleting)
+	router.PATCH("/somePatching", patching)
+	router.HEAD("/someHead", head)
+	router.OPTIONS("/someOptions", options)
+	err := router.Run()
+	if err != nil {
+		return
+	}
 }
