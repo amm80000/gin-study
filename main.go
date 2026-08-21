bf85802538f29d9376ffbc33bfbbe56a0f20b9dd
an'zhong'yuan <919423510@qq.com>
2026-08-21 16:06:38 +0800

    [FEAT]基本分组

--- main.go
+++ main.go
@@ -1,42 +1,49 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"net/http"
-	"path/filepath"
 
 	"github.com/gin-gonic/gin"
 )
+
+func loginEndpoint(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"action": "login",
+	})
+}
+
+func submitEndpoint(c *gin.Context) {
+
+	c.JSON(http.StatusOK, gin.H{
+		"action": "submit",
+	})
+}
+
+func readEndpoint(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"action": "read",
+	})
+}
 
 func main() {
 
 	router := gin.Default()
 
-	router.MaxMultipartMemory = 8 << 20
+	// Simple group: v1
+	{
+		v1 := router.Group("/v1")
+		v1.POST("/login", loginEndpoint)
+		v1.POST("/submit", submitEndpoint)
+		v1.POST("/read", readEndpoint)
+	}
 
-	router.POST("/upload", func(c *gin.Context) {
-
-		form, err := c.MultipartForm()
-
-		if err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
-
-		files := form.File["files"]
-
-		for _, file := range files {
-
-			log.Println(file.Filename)
-
-			dst := filepath.Join("./files/", filepath.Base(file.Filename))
-
-			c.SaveUploadedFile(file, dst)
-
-			c.String(http.StatusOK, fmt.Sprintf("'%s' files uploaded!", file.Filename))
-		}
-	})
+	// Simple group: v2
+	{
+		v2 := router.Group("/v2")
+		v2.POST("/login", loginEndpoint)
+		v2.POST("/submit", submitEndpoint)
+		v2.POST("/read", readEndpoint)
+	}
 
 	err := router.Run()
 	if err != nil {
